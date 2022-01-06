d4f2176bfc0546e0e0d347a96b01d6814a0d34de
Arasi Arumugam <ezhil.arum@gmail.com>
2022-01-05 22:04:35 -0600

    Authentication Filter created

--- src/main/java/org/launchcode/lendinglibrary/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/lendinglibrary/controllers/AuthenticationController.java
@@ -2,8 +2,10 @@ package org.launchcode.lendinglibrary.controllers;
 
 import org.launchcode.lendinglibrary.models.User;
 import org.launchcode.lendinglibrary.models.data.UserRepository;
+import org.launchcode.lendinglibrary.models.dto.LoginFormDTO;
 import org.launchcode.lendinglibrary.models.dto.RegisterFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.Optional;
 
+@Controller
 public class AuthenticationController {
     @Autowired
     UserRepository userRepository;
@@ -75,5 +78,48 @@ public class AuthenticationController {
         setUserInSession(request.getSession(), newUser);
 
         return "redirect:";
+    }
+
+    @GetMapping("/login")
+    public String displayLoginForm(Model model) {
+        model.addAttribute(new LoginFormDTO());
+        model.addAttribute("title", "Log In");
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String processLoginForm(@ModelAttribute @Valid LoginFormDTO loginFormDTO,
+                                   Errors errors, HttpServletRequest request,
+                                   Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Log In");
+            return "login";
+        }
+
+        User theUser = userRepository.findByUsername(loginFormDTO.getUsername());
+
+        if (theUser == null) {
+            errors.rejectValue("username", "user.invalid", "The given username does not exist");
+            model.addAttribute("title", "Log In");
+            return "login";
+        }
+
+        String password = loginFormDTO.getPassword();
+
+        if (!theUser.isMatchingPassword(password)) {
+            errors.rejectValue("password", "password.invalid", "Invalid password");
+            model.addAttribute("title", "Log In");
+            return "login";
+        }
+
+        setUserInSession(request.getSession(), theUser);
+
+        return "redirect:";
+    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request){
+        request.getSession().invalidate();
+        return "redirect:/login";
     }
 }
