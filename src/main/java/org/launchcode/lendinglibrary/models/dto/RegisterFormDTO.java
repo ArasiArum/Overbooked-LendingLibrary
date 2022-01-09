6900f2e984cbbebfd4575303eb9adb1bad0fc489
Arasi Arumugam <ezhil.arum@gmail.com>
2022-01-09 16:34:11 -0600

    email field added in user registration

--- src/main/java/org/launchcode/lendinglibrary/models/dto/RegisterFormDTO.java
+++ src/main/java/org/launchcode/lendinglibrary/models/dto/RegisterFormDTO.java
@@ -1,5 +1,9 @@
 package org.launchcode.lendinglibrary.models.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class RegisterFormDTO extends LoginFormDTO {
 
     private String verifyPassword;
@@ -12,4 +16,18 @@ public class RegisterFormDTO extends LoginFormDTO {
     public void setVerifyPassword(String verifyPassword){
         this.verifyPassword = verifyPassword;
     }
+
+    @NotNull
+    @NotBlank
+    @Email(message = "Invalid email address")
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
+
