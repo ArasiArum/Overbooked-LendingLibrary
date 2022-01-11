88c1eb4fb18a020e4e100888926de269447547b7
Arasi Arumugam <ezhil.arum@gmail.com>
2022-01-10 20:51:21 -0600

    Genre controller created

--- src/main/java/org/launchcode/lendinglibrary/models/Book.java
+++ src/main/java/org/launchcode/lendinglibrary/models/Book.java
@@ -1,8 +1,6 @@
 package org.launchcode.lendinglibrary.models;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 
@@ -19,19 +17,19 @@ public class Book extends AbstractEntity{
     @Size(min = 5 , max = 60, message= "Author name must be between 5 and 60 characters!")
     private String author;
 
-    @ManyToOne
-    @NotNull(message = "Genre is required")
-    private Genre genre;
+//    @ManyToOne
+//    @NotNull(message = "Genre is required")
+//    private Genre genre;
 
 
     @Size(max=500 , message = "Summary should not exceed 400 characters!")
     private String summary;
 
-    public Book(String name, String author, Genre genre, String summary){
+    public Book(String name, String author, String summary){
 
         this.name = name;
         this.author = author;
-        this.genre = genre;
+       // this.genre = genre;
         this.summary = summary;
 
     }
@@ -61,11 +59,11 @@ public class Book extends AbstractEntity{
         this.summary = summary;
     }
 
-    public Genre getGenre() {
-        return genre;
-    }
-
-    public void setGenre(Genre genre) {
-        this.genre = genre;
-    }
+//    public Genre getGenre() {
+//        return genre;
+//    }
+//
+//    public void setGenre(Genre genre) {
+//        this.genre = genre;
+//    }
 }
--- src/main/java/org/launchcode/lendinglibrary/models/Genre.java
+++ src/main/java/org/launchcode/lendinglibrary/models/Genre.java
@@ -1,10 +1,7 @@
 package org.launchcode.lendinglibrary.models;
 
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 public class Genre extends AbstractEntity{
@@ -12,8 +9,7 @@ public class Genre extends AbstractEntity{
     @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
 
-    @OneToMany(mappedBy = "genre")
-    private final List<Book> books = new ArrayList<>();
+
 
     public Genre(@Size(min = 3, message = "Name must be at least 3 characters long") String name) {
         this.name = name;
@@ -29,9 +25,9 @@ public class Genre extends AbstractEntity{
         this.name = name;
     }
 
-    public List<Book> getBooks() {
-        return books;
-    }
+//    public List<Book> getBooks() {
+//        return books;
+//    }
 
     @Override
     public String toString() {
